741aaea6b0f81e0dc4e6e94a2931ae8773d1a530
Kirill Kostarev <kirchberg@yandex-team.ru>
2023-02-25 17:17:38 +0300

    Finished laboratory work 2

---
+++ LR2/src/main/kotlin/Main.kt
@@ -0,0 +1,4 @@
+fun main(args: Array<String>) {
+//    task10V1()
+    task10V2()
+}
---
+++ LR2/src/main/kotlin/Task 2.kt
@@ -0,0 +1,100 @@
+import java.time.LocalDateTime
+import kotlin.random.Random
+
+// Variant №1
+// Task №10
+
+/*
+№10. Необходимо вывести внизу фамилию разработчика, дату и время получения задания, а также дату и время сдачи задания.
+Для получения последней даты и времени следует использовать класс Date.
+Используя оператор switch, написать программу, которая выводит на экран сообщения о принадлежности некоторого значения
+k интервалам (-10k, 5], [0, 10], [5, 15], [10, 10k].
+ */
+fun task10V1() {
+    println(message = "Task 1 Var. 1")
+
+    val jobReceivedTime = LocalDateTime.now()
+
+    print(message = "Enter your developer name: ")
+    val developerName = readlnOrNull() ?: return
+
+    print(message = "Enter k: ")
+    var k = readlnOrNull()?.toIntOrNull() ?: return
+
+    when (k) {
+        in -10 * k..5 -> println("$k belongs to the interval (-10000, 5]")
+        in 0..10 -> println("$k belongs to the interval [0, 10]")
+        in 5..15 -> println("$k belongs to the interval [5, 15]")
+        in 10..10000 -> println("$k belongs to the interval [10, 10000]")
+        else -> println("$k does not belong to any interval")
+    }
+
+    val jobCompletedTime = LocalDateTime.now()
+    println(message = "Developer name: $developerName")
+    println(message = "Job received time: $jobReceivedTime")
+    println(message = "Job completed time: $jobCompletedTime")
+}
+
+// Variant №2
+// Task №10
+
+/*
+№10. Ввести с консоли n – размерность матрицы a[n][n]. Задать значения элементов матрицы в интервале значений от -n до n
+с помощью датчика случайных чисел.
+Найти максимальный элемент(ы) в матрице и удалить из матрицы все строки и столбцы, его содержащие
+ */
+fun task10V2() {
+
+    fun printMatrix(a: Array<IntArray>) {
+        for (i in a.indices) {
+            for (j in a[i].indices) {
+                print("${a[i][j]}\t")
+            }
+            println()
+        }
+    }
+
+    // Read the dimensionality of the matrix from the console
+    print("Enter the dimensionality of the matrix: ")
+    val n = readLine()?.toInt() ?: 0
+
+    // Initialize the matrix with random values in the range [0, n]
+    val matrix = Array(n) { i ->
+        IntArray(n) { j ->
+            Random.nextInt(-n, n)
+        }
+    }
+
+    // Find the maximal element(s) in the matrix
+    var maxElement = Int.MIN_VALUE
+    val maxElements = mutableListOf<Pair<Int, Int>>()
+    for (i in 0 until n) {
+        for (j in 0 until n) {
+            if (matrix[i][j] > maxElement) {
+                maxElement = matrix[i][j]
+                maxElements.clear()
+                maxElements.add(Pair(i, j))
+            } else if (matrix[i][j] == maxElement) {
+                maxElements.add(Pair(i, j))
+            }
+        }
+    }
+
+    // Delete rows and columns containing the maximal element(s)
+    val rowsToDelete = maxElements.map { it.first }.toSet()
+    val colsToDelete = maxElements.map { it.second }.toSet()
+    val newMatrix = Array(n - rowsToDelete.size) { i ->
+        IntArray(n - colsToDelete.size) { j ->
+            val row = if (i < rowsToDelete.first()) i else i + 1
+            val col = if (j < colsToDelete.first()) j else j + 1
+            matrix[row][col]
+        }
+    }
+
+    // Print the original matrix and the modified matrix
+    println("Original matrix:")
+    printMatrix(matrix)
+    println("Modified matrix:")
+    printMatrix(newMatrix)
+
+}
